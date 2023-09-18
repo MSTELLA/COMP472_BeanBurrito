874815653d4faa61bba9177b32860bafb47050e8
MSTELLA <m_stella@live.concordia.ca>
2023-09-18 15:51:18 -0400

    Separated AI_wargame into multiple files - import not checked

---
+++ Types.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+import argparse
+import copy
+from datetime import datetime
+from enum import Enum
+from dataclasses import dataclass, field
+from time import sleep
+from typing import Tuple, TypeVar, Type, Iterable, ClassVar
+import random
+import requests
+
+# maximum and minimum values for our heuristic scores (usually represents an end of game condition)
+MAX_HEURISTIC_SCORE = 2000000000
+MIN_HEURISTIC_SCORE = -2000000000
+
+class UnitType(Enum):
+    """Every unit type."""
+    AI = 0
+    Tech = 1
+    Virus = 2
+    Program = 3
+    Firewall = 4
+
+class Player(Enum):
+    """The 2 players."""
+    Attacker = 0
+    Defender = 1
+
+    def next(self) -> Player:
+        """The next (other) player."""
+        if self is Player.Attacker:
+            return Player.Defender
+        else:
+            return Player.Attacker
+
+class GameType(Enum):
+    AttackerVsDefender = 0
+    AttackerVsComp = 1
+    CompVsDefender = 2
+    CompVsComp = 3
