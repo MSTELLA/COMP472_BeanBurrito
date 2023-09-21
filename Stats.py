189825bb06f002612ab43d4451f3223beff3195b
guidote <gabrielle.guidote@gmail.com>
2023-09-21 16:24:24 -0400

    remove some files

--- Stats.py
+++
@@ -1,7 +0,0 @@
-
-@dataclass(slots=True)
-
-class Stats:
-    """Representation of the global game statistics."""
-    evaluations_per_depth : dict[int,int] = field(default_factory=dict)
-    total_seconds: float = 0.0
